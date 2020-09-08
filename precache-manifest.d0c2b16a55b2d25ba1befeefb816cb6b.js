73d87a05d733652fb51b0a5d8a9a5b75fd8f6c9d
isuchit <suchitrotti@gmail.com>
2020-09-08 21:01:47 +0530

    add build

---
+++ precache-manifest.d0c2b16a55b2d25ba1befeefb816cb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7b89e8ee217a08f766b6810435b6c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b55570051aab943e406",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b55570051aab943e406",
+    "url": "/static/js/main.f457c227.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
